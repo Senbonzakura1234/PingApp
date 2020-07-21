856b775556640f54f95f506b0d35b98e1d16f813
Senbonzakura <anhdungpham090@gmail.com>
2020-07-21 18:55:50 +0700

    initial commit.

---
+++ src/main/java/com/app/spring/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.app.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    //    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
